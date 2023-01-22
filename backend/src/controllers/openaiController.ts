7bd2dc815915ae3462c682639afd39e42998bea3
fernandobouchet <fernandobouchet@gmail.com>
2023-01-22 15:12:45 -0300

    Create Route,Controller with the logic to send a 'POST' request to the 'openai' API to get an image based on the body 'prompt' and 'size'

---
+++ backend/src/controllers/openaiController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const generateImage = async (req: Request, res: Response) => {
+  try {
+    const { prompt, size } = req.body;
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: size,
+    });
+
+    const image = response.data.data[0].url;
+
+    res.status(200).send({
+      success: true,
+      data: image,
+    });
+  } catch (error: any) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+    res.status(400).send({
+      error: error,
+    });
+  }
+};
+
+export { generateImage };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import openaiRoute from './routes/openaiRoute';
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/generateImage', openaiRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
